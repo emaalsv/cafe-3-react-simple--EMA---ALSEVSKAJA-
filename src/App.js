8e9311e116583deb24e5d5616a343d878bdf028e
Ema Alsv <alsevskaja.ema@gmail.com>
2022-07-18 09:11:57 +0300

    started router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
 import Home from './Home/Home';
-import Header from './Header/Header'
+import Header from './Header/Header';
+import {BrowserRouter,
+        Routes,
+        Route} from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" />
+    </Routes>
     <div className="app">
       <Header />
       <Home />
     </div>
+    </BrowserRouter>
   );
 }
 
