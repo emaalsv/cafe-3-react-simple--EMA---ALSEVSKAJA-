6d34fb2aa1461601ef8a64ab543b14433403cdf9
Ema Alsv <alsevskaja.ema@gmail.com>
2022-07-18 08:02:31 +0300

    Sukurti keli puslapiai, pradedamas daryti headeris kai bus paruostas dizainas

---
+++ src/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <img className="header-icon"
+            src=""
+            alt="Icon"
+            />
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Login/Signup/Log.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Log.css'
+
+function Log() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Log
