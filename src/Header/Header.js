d5538f61b05468e3681e148251f21b8df5e69645
Ema Alsv <alsevskaja.ema@gmail.com>
2022-07-18 08:54:59 +0300

    added logos to media, need to resolve a bug in header which is commented out

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
-import './Header.css'
-
+import './Header.css';
+// import logo from './media/logo.png'
 function Header() {
     return (
         <div className="header">
             <img className="header-icon"
-            src=""
-            alt="Icon"
+            // src={logo}
+            alt="Logo"
             />
         </div>
     )
 }
 
 export default Header
+
+// Sutvarkyti paveiksliuko ikelima
